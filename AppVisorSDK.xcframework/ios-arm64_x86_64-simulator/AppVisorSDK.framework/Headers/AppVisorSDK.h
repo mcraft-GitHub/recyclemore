18739241a6f3b5c51480562c737ee0cb3c50dc87
貸し出し用 <>
2025-10-17 15:00:01 +0900

    AppVisorをプロジェクト内に移動

---
+++ AppVisorSDK.xcframework/ios-arm64_x86_64-simulator/AppVisorSDK.framework/Headers/AppVisorSDK.h
@@ -0,0 +1,18 @@
+//
+//  AppVisorSDK.h
+//  AppVisorSDK
+//
+//  Created by Erick Gonzales on 26/4/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppVisorSDK.
+FOUNDATION_EXPORT double AppVisorSDKVersionNumber;
+
+//! Project version string for AppVisorSDK.
+FOUNDATION_EXPORT const unsigned char AppVisorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppVisorSDK/PublicHeader.h>
+
+
