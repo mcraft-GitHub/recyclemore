8036d52e27cff696084671ee3af7bc8d28f1d1e6
貸し出し用 <>
2025-10-20 17:00:21 +0900

    初回ログインAPIの追加とログイン欄入力エラー時のパディングの制御処理を追加

--- recyclemore/recyclemore/LoginView.swift
+++ recyclemore/recyclemore/LoginView.swift
@@ -25,6 +25,7 @@ struct LoginView: View {
     @State private var modalType:ModalType = .close
     
     @State private var topPadding: CGFloat = 120 // メインコンテンツパディング高
+    @State private var topPaddingOffset: CGFloat = 0 // パディング調整値
     
     var body: some View {
         if isLoading {
@@ -76,7 +77,8 @@ struct LoginView: View {
                             
                             // フォーム領域
                             VStack{
-                                LoginInputSectionView(email: $email, password: $password,emailError: emailErrorMessage,passwordError: passwordErrorMessage)
+                                LoginInputSectionView(email: $email, password: $password,currentView: $currentView,emailError: emailErrorMessage,passwordError: passwordErrorMessage
+                                    )
                             }
                             .frame(maxWidth: .infinity)
                             .padding(.top)
@@ -100,12 +102,14 @@ struct LoginView: View {
                                 
                                 Button(action: {
                                     Task {
+                                        topPaddingOffset = 0
                                         if email != "" {
                                             emailErrorMessage = nil
                                         }
                                         else
                                         {
                                             emailErrorMessage = "未入力"
+                                            topPaddingOffset += 10
                                         }
                                         
                                         if password != "" {
@@ -114,6 +118,7 @@ struct LoginView: View {
                                         else
                                         {
                                             passwordErrorMessage = "未入力"
+                                            topPaddingOffset += 10
                                         }
                                         
                                         // 両方入力されていればログインAPIを実行
@@ -140,7 +145,7 @@ struct LoginView: View {
                         }
                         .padding(.horizontal, 20)
                         .padding(.vertical, 40)
-                        .padding(.top,topPadding)
+                        .padding(.top,topPadding-topPaddingOffset)
                         .frame(maxWidth: .infinity)
                     }
                 }
@@ -158,6 +163,8 @@ struct LoginView: View {
                         })
                     case .back:
                         ErrorModalView(isShowingModal: $isShowingModal,messag: errorMessage,code: errorCode)
+                    default:
+                        EmptyView()
                     }
                 }
             }
@@ -238,16 +245,109 @@ struct LoginView: View {
                 await MainActor.run {
                     print("ログイン失敗")
                     // TODO:エラーコードに応じてエラーモーダルを分岐する
-                    /*
                     errorMessage = "ログインに失敗しました\nメールアドレスとパスワードをお確かめください"
                     errorCode = "エラーコード：001"
                     modalType = .close
-                    isShowingModal = true*/
-                    
+                    isShowingModal = true
+                    /*
                     print("仮成功模擬")
                     MultiViewURL = "https://dev5.m-craft.com/harada/mc_kadai/SwiftTEST/WebViewtest2.php"
                     currentView = .web
-                     
+                    */
+                }
+            }
+        }
+        catch {
+            await MainActor.run {
+                print("通信自体失敗")
+                // TODO:エラーモーダルを表示する
+                isShowingModal = true
+            }
+        }
+    }
+    
+    func InitialLoginAPI() async {
+        // 初回ログインAPI
+        guard let url = URL(string: "https://api-recyclemore-cafzh7ewbngsdreu.japaneast-01.azurewebsites.net/v1/Auth/initial-login")
+        else
+        {
+            return
+        }
+        
+        // 送信データ
+        let params = [
+            "mail":initial_email,
+            "initial_token":initial_token,
+            "token":Appvisor.appvisorUDID(),
+            "OS":"iOS",
+        ] as [String:Any]
+        
+        do {
+            //リクエストデータの作成
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue(API_KEY, forHTTPHeaderField: "x-recyclemore-api-key")
+            
+            //ボディにJsonをセット
+            request.httpBody = try JSONSerialization.data(withJSONObject: params, options: [])
+            
+            //通信を実行
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            //ステータスコードの確認
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("HTTPレスポンスじゃないので中断")
+                return
+            }
+            
+            print("ステータスコード", httpResponse.statusCode)
+            
+            //レスポンスの表示
+            if let jsonString = String(data: data, encoding: .utf8){
+                await MainActor.run {
+                    print(jsonString)
+                }
+            }
+            
+            // ログインに成功していれば
+            if httpResponse.statusCode == 200
+            {
+                if let decoded = try? JSONDecoder().decode(LoginResponse.self, from: data) {
+                    await MainActor.run{
+                        // 結果からユーザー情報は作れない
+                        // ログインに成功した情報を端末に保存する
+                        KeychainHelper.shared.save(Appvisor.appvisorUDID(), key: "token")
+                        KeychainHelper.shared.save(initial_email, key: "email")
+                        
+                        // 表示する画面を切り替える
+                        // TODO:URLも切り替える
+                        currentView = .web
+                    }
+                }
+                else
+                {
+                    await MainActor.run {
+                        print("デコード失敗")
+                        // TODO:エラーモーダルを表示する
+                        isShowingModal = true;
+                    }
+                }
+            }
+            else
+            {
+                await MainActor.run {
+                    print("ログイン失敗")
+                    // TODO:エラーコードに応じてエラーモーダルを分岐する
+                    errorMessage = "ログインに失敗しました\nメールアドレスとパスワードをお確かめください"
+                    errorCode = "エラーコード：001"
+                    modalType = .close
+                    isShowingModal = true
+                    /*
+                    print("仮成功模擬")
+                    MultiViewURL = "https://dev5.m-craft.com/harada/mc_kadai/SwiftTEST/WebViewtest2.php"
+                    currentView = .web
+                    */
                 }
             }
         }
